088f0eb04edd0da3f8572acece47291a0303625b
MohamedElshewy <abdalluhmohamed@gmail.com>
2025-12-05 22:32:41 +0200

    add BitOps class with bit manipulation methods

---
+++ src/main/java/org/example/BitOps.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class BitOps {
+
+
+    public int getBit(int number, int position) {
+        return (number >> (position)) & 1;
+    }
+
+    public int setBit(int number, int position) {
+        return (1 << position) | number;
+    }
+
+
+    public int clearBit(int number, int position) {
+        return number & ~(1 << position);
+    }
+
+    public int updateBit(int number, int position, boolean value) {
+        if (value) {
+            return (1 << position) | number;
+        } else {
+            return number & ~(1 << position);
+        }
+    }
+}
